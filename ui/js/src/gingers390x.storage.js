2532571d9f251070bfc2e33bb831a1b36594a409
Atreyee Mukhopadhyay <atreyee@linux.vnet.ibm.com>
2015-12-14 17:45:33 +0530

    Changing implementation of Add all and add selected based on the luns scan status

--- ui/js/src/gingers390x.storage.js
+++ ui/js/src/gingers390x.storage.js
@@ -1,49 +1,70 @@
 gingers390x.initFCPLunsDetails = function(){
   gingers390x.loadFCPLunsList();
 
-    $('#discoverLuns').on("click",function(){
-      wok.message.warn("Searching Luns ",'#alert-modal-storage-container');
-
-   		var taskAccepted = false;
-   			var onTaskAccepted = function() {
-   					if(taskAccepted) {
-   							return;
-   					}
-   					taskAccepted = true;
-   					//wok.topic('gingers390x/enableNetworks').publish();
-   		 };
-      gingers390x.lunsDiscovery(function(result){
-          onTaskAccepted();
-          gingers390x.retrieveLunsList();  //Reload The list
-          var successText = result['message'];
-          gingers390x.messagecloseable.success(successText,'#alert-modal-storage-container');
-        //  wok.topic('gingers390x/enableNetworks').publish();
-      },function(result){
-          gingers390x.retrieveLunsList();;  //Reload the list
-          if (result['message']) { // Error message from Async Task status TODO
-              var errText = result['message'];
-          }
-          else { // Error message from standard gingers390x exception TODO
-              var errText = result['responseJSON']['reason'];
-          }
-          result && gingers390x.messagecloseable.error(errText,'#alert-modal-storage-container');
-          taskAccepted;
-       },onTaskAccepted);
-  });
+    $('#refreshLuns').on("click",function(){
+       gingers390x.retrieveLunsList();
+     });
 
   $("#enableLunsScan").on("click",function(){
       gingers390x.getLunsScanStatus(function(result){
-        alert(result.current);
          gingers390x.lunsScanStatusChange(result.current,function(response){
-           alert(JSON.stringify(response));
-           var lunsStatusButtonText = (response.current)?'Disable LUN Scan':'Enable LUN Scan';
+            var lunsStatusButtonText ,messageText ="";
+           if(response.current){
+             lunsStatusButtonText = 'Disable LUN Scan';
+             messageText = 'Luns Scan is enabled successfully';
+           }else{
+             lunsStatusButtonText = 'Enable LUN Scan';
+             messageText = 'Luns Scan disabled successfully';
+           }
+             gingers390x.messagecloseable.success(messageText,'#alert-modal-storage-container');
              $('#enableLunsScan').text(lunsStatusButtonText);
+         },function(result){
+           gingers390x.messagecloseable.error("LUNs scan enable/disable failed",'#alert-modal-storage-container');
          });
+     },function(result){
+         gingers390x.messagecloseable.error("LUNs scan status unavailable",'#alert-modal-storage-container');
     });
   });
 
   gingers390x.getLunsScanStatus(function(result){
-    var lunsStatusButtonText = (result.current)?'Disable LUN Scan':'Enable LUN Scan';
+    var lunsStatusButtonText = "";
+      if(result.current){
+        lunsStatusButtonText = 'Disable LUN Scan';
+        $('luns-add-all-button').text("Add all / Luns Scan");
+        $('luns-add-selected-button').hide();
+        $('luns-add-all-button').on("click",function(){
+              wok.message.warn("Searching Luns ",'#alert-modal-storage-container');
+       var taskAccepted = false;
+         var onTaskAccepted = function() {
+             if(taskAccepted) {
+                 return;
+             }
+             taskAccepted = true;
+             //wok.topic('gingers390x/enableNetworks').publish();
+        };
+       gingers390x.lunsDiscovery(function(result){
+           onTaskAccepted();
+           gingers390x.retrieveLunsList();  //Reload The list
+           var successText = result['message'];
+           gingers390x.messagecloseable.success(successText,'#alert-modal-storage-container');
+         //  wok.topic('gingers390x/enableNetworks').publish();
+       },function(result){
+           gingers390x.retrieveLunsList();;  //Reload the list
+           if (result['message']) { // Error message from Async Task status TODO
+               var errText = result['message'];
+           }
+           else { // Error message from standard gingers390x exception TODO
+               var errText = result['responseJSON']['reason'];
+           }
+           result && gingers390x.messagecloseable.error(errText,'#alert-modal-storage-container');
+           taskAccepted;
+        },onTaskAccepted);
+        });
+      }else{
+        lunsStatusButtonText = 'Enable LUN Scan';
+        $('luns-add-all-button').text("Add all");
+        $('luns-add-selected-button').show();
+      }
       $('#enableLunsScan').text(lunsStatusButtonText);
 
   });
@@ -65,19 +86,25 @@ gingers390x.loadFCPLunsList = function(){
     "column-id":'remoteWwpn',
     'display-name':'Remote Wwpn',
     "type": 'string',
-    "width":"30%"
+    "width":"25%"
+  },
+  {
+    "column-id": 'lunId',
+    'display-name':'Lun Id',
+    "type": 'string',
+    "width":"20%"
+  },
+  {
+    "column-id": 'product',
+    'display-name':'SAN Controller',
+    "type": 'string',
+     "width":'15%'
   },
   {
     "column-id": 'controllerSN',
     'display-name':'SAN Controller Id/Serial No',
     "type": 'string',
-     "width":'30%'
-  },
-    {
-    "column-id": 'lunId',
-    'display-name':'Lun Id',
-    "type": 'string',
-    "width":"20%"
+     "width":'20%'
   },
   {
     "column-id":'Srno',
@@ -101,7 +128,7 @@ gingers390x.addFCPActions=function(){
   opts['gridId'] = 'fcp-luns-table-grid';
 
   var actionButton = [{
-          id:'add-selected-button',
+          id:'luns-add-selected-button',
           class: 'fa fa-plus-circle',
           label: 'Add Selected',
           onClick: function(event) {
@@ -144,10 +171,10 @@ gingers390x.addFCPActions=function(){
 
       },
       {
-          id:'add-all-button',
+          id:'luns-add-all-button',
           class: 'fa fa-plus-circle',
-          label: 'Add All',
-          onClick: function(event) {
+          label: 'Add All'
+          /*,onClick: function(event) {
             var selectedRowDetails = gingers390x.getCurrentRows(opts);
             var rowIndex = 0;
              var failedlLuns = [];
@@ -181,7 +208,7 @@ gingers390x.addFCPActions=function(){
              gingers390x.messagecloseable.error("Error occured while adding Luns",'#alert-modal-storage-container');
           }
              gingers390x.retrieveLunsList();
-          }
+          }*/
       }];
 
       var actionListSettings = {
